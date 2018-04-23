8235ffc409f7fe19d8c7a62a2f37f03e9f4cb42a
XyloRekindled <uprashanth94@outlook.com>
2018-04-23 23:33:23 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -35,19 +35,31 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     
-    if(!ObjectId.isValid(id))
-    {
-        return res.status(404).send();
-    }
+    if(!ObjectId.isValid(id)) return res.status(404).send();
 
     Todo.findById(id)
         .then((todo) => {
-            if(!todo) return res.status(40).send();
+            if(!todo) return res.status(404).send();
             res.send({todo});
         })
         .catch((e) => {
-            res.status(404).send();
+            res.status(400).send();
         })
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) { return res.status(404).send(); }
+
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+            if(!todo) return res.status(404).send();
+            res.send(todo);
+        })
+        .catch((e) => {
+            res.status(400).send();
+        });
 });
 
 app.listen(port, () => {
