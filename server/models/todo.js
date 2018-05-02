c6d1842d27c45682ad226fbb6ab66b609e6a7884
XyloRekindled <uprashanth94@outlook.com>
2018-05-02 08:28:21 +0530

    final code

--- server/models/todo.js
+++ server/models/todo.js
@@ -17,6 +17,11 @@ var Todo = mongoose.model('Todo', {
     {
         type: Number,
         default: null
+    },
+    _creator:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ const port = process.env.PORT;
 
 app.use(bodypaser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-        text:req.body.text
+        text:req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -27,21 +28,24 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find()
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({_creator: req.user._id})
         .then((todos) => {
             res.send({todos});
         }, (e) => {
             res.status(400).send(e);
-        })
+        });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     
     if(!ObjectId.isValid(id)) return res.status(404).send();
 
-    Todo.findById(id)
+    Todo.findOne({
+            _id:id,
+            _creator: req.user._id
+        })
         .then((todo) => {
             if(!todo) return res.status(404).send();
             res.send({todo});
